a5387e00edb6efa4885b07107606a958a2fa7639
NAKANISHI Kiyomaro <kiyomaro092@gmail.com>
2024-12-24 19:58:06 +0900

    Fix detection of usb tx buffer overflow

--- src/usbd_cdc_if.c
+++ src/usbd_cdc_if.c
@@ -250,21 +250,7 @@ void cdc_process(void)
 void cdc_transmit(uint8_t* buf, uint16_t len)
 {
     system_irq_disable();
-    if( ((txbuf.head + len) % USBTXQUEUE_LEN) == txbuf.tail)    // FIXME no error if size of enqueued data > size of empty area
-    //
-    // txbuf.tail: First in data = First out data
-    // head: Head of empty area = Last in data + 1
-    //
-    // data count in q: (txbuf.head - txbuf.tail + USBTXQUEUE_LEN) % USBTXQUEUE_LEN
-    // size of q: USBTXQUEUE_LEN - 1U // we cannot set tail = head. it's empty.
-    //
-    // usable size: "size of q" - "data count in q"
-    //            = (USBTXQUEUE_LEN - 1U) - (txbuf.head - txbuf.tail + USBTXQUEUE_LEN) % USBTXQUEUE_LEN
-    //            = (USBTXQUEUE_LEN - 1U - txbuf.head + txbuf.tail) % USBTXQUEUE_LEN
-    //
-    // It should be like
-    //if (len > (USBTXQUEUE_LEN - 1U - txbuf.head + txbuf.tail) % USBTXQUEUE_LEN)
-    // ?
+    if (len > (USBTXQUEUE_LEN + txbuf.tail - txbuf.head - 1U) % USBTXQUEUE_LEN)
     {
         error_assert(ERR_FULLBUF_USBTX);
     }
